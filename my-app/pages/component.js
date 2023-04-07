17a130a1a80a43bdcbe23c480cbb034559827d2c
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-04-07 15:22:29 +0500

    componenet editted

--- my-app/pages/component.js
+++ my-app/pages/component.js
@@ -1,25 +1,38 @@
-export  default function Component(){
+// import { useState } from "react";
+import  { useState } from 'react';
+// const [conut, setCount] = useState(0)
+
+
+export default function Component() {
+    const [count, setCount] = useState(0);
     return (
-        <h1>this is an external Component</h1>
+        <>
+            <h1>this is an external Component witha a state number : {count}</h1>
+            <button onClick={()=>
+                    setCount((prev)=>prev+1)
+            }>
+                State Update
+            </button>
+        </>
     )
 }
 
 
-export function SecondComponenet({name}){
+export function SecondComponenet({ name }) {
     return (
         <>
-        
-            <Time/>
+
+            <Time />
             <h1>
                 I am  Second Component from {name}
             </h1>
         </>
     )
-}   
+}
 
 
-function Time (){
-    return(
+function Time() {
+    return (
         <h1>Nested Component</h1>
     )
 }
