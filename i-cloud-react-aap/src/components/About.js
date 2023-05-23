478b0a2257f47e9fa951403a76f947b0ea45bf79
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-05-23 20:56:30 +0500

    filter

--- i-cloud-react-aap/src/components/About.js
+++ i-cloud-react-aap/src/components/About.js
@@ -1,38 +1,45 @@
 import noteContext from '../context/notes/noteContext';
-import React, { useContext , useEffect} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AllListings from './AllListings';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function About() {
-   
+
 
     let history = useNavigate()
     const context = useContext(noteContext)
     const { allListings } = context
     useEffect(() => {
         console.log('i fire once');
-        if (!localStorage.getItem('token')) 
+        if (!localStorage.getItem('token'))
             history("/login")
-            
-    },[]) // eslint-disable-line react-hooks/exhaustive-deps
 
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-    
+    const [range, setRange] = useState(0)
+    const onChange =(e)=>{
+        setRange({[e.target.name]: e.target.value})
+            console.log(range)  
+    }
+
 
     return (
-        
-                <div className='row my-3'>
-                    <h2>All Listings </h2>
-        
-                    {allListings.map((note) => (
+        <>
+            <label htmlFor="customRange1" className="form-label">Price Range</label>
+            <input type="range" className="form-range"  min="0" max="100"id="customRange1" onChange={onChange}></input>
+            <div className='row my-3'>
+                <h2>All Listings </h2>
 
-                         <AllListings key={note._id} note={note}/>
-                    ))
-                    }
-                    
-                </div>
+                {allListings.map((note) => (
+
+                    <AllListings key={note._id} note={note} range={range} />
+                ))
+                }
+
+            </div>
+        </>
 
 
-            )}
-            
+    )
+}
