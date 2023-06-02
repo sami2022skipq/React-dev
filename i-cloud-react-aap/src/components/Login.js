f6eb558fe58a94d2c181c0e2966c7bb219840709
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-06-02 21:33:07 +0500

    Show password

--- i-cloud-react-aap/src/components/Login.js
+++ i-cloud-react-aap/src/components/Login.js
@@ -2,8 +2,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 const Login = (props) => {
     const [credentials, setCredentials] = useState({ email: "", password: "" })
-    let {email, password} = credentials
-    email=email.toLocaleLowerCase()
+    let { email, password } = credentials
+    email = email.toLocaleLowerCase()
     let history = useNavigate()
 
     const handelsubmit = async (e) => {
@@ -20,17 +20,23 @@ const Login = (props) => {
         console.log(json)
         if (json.success) {
             // save the auth token and redirect
-            localStorage.setItem('token', json.authToken )
+            localStorage.setItem('token', json.authToken)
             props.showAlert("Logged in  successfully", "success")
-            
+
             history("/")
         }
         else {
-           props.showAlert("Invalid username or password", "danger")
+            props.showAlert("Invalid username or password", "danger")
         }
     }
     const onChange = (e) => {
         setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+    const [toggleSowPassword, setToggleSowPassword] = useState(false)
+    const toggle = () => {
+        
+        setToggleSowPassword(!toggleSowPassword)
+
     }
 
 
@@ -38,7 +44,7 @@ const Login = (props) => {
         <div>
             <div className=" my-2">
                 <h3>
-                Enter your Login details 
+                    Enter your Login details
                 </h3>
             </div>
             <form onSubmit={handelsubmit}>
@@ -47,12 +53,20 @@ const Login = (props) => {
                     <input type="email" className="form-control" id="email" name="email" value={credentials.email} aria-describedby="emailHelp" onChange={onChange} required />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div className="mb-3">
+                <div className="mb-1">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} required />
+                    <input type={toggleSowPassword ? "text" : "password"} className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} required />
+                </div>
+                <div className="form-check mb-3">
+                    <input className="form-check-input" type="checkbox" value="password" id="showPassword" onChange={toggle} />
+                    <label className="form-check-label" >
+                        Show Password
+                    </label>
                 </div>
 
-                <button type="submit" className="btn btn-primary">Login</button>
+                <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </div>
             </form>
         </div>
     )
